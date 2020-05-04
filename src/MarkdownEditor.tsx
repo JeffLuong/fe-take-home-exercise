5cf087f116941ef48b47bfa9c9e978c620da014d
Jeffrey Luong <jjck1988@gmail.com>
2020-05-04 15:37:26 -0400

    Use new hooks to add convenience functionality

--- src/MarkdownEditor.tsx
+++ src/MarkdownEditor.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
-import { useDebounce } from './hooks';
+import { useDebounce, useUndoRedo, useEventListener } from './hooks';
 
 import MarkdownPreview from './MarkdownPreview';
 
@@ -24,18 +24,57 @@ type MarkdownEditorProps = {
     placeholder?: string;
 };
 
+// Need to extend Event because KeyboardEvent is not compatible with Event
+interface CustomKBEvent extends Event {
+    ctrlKey?: boolean;
+    metaKey?: boolean;
+    shiftKey?: boolean;
+    key?: string;
+}
+
+const getCommand = (e: CustomKBEvent): string => {
+    const { ctrlKey, metaKey, shiftKey, key } = e;
+    if (ctrlKey || metaKey) {
+        if (key === 'z') {
+            return shiftKey ? 'redo' : 'undo';
+        }
+        return '';
+    }
+    return '';
+};
+
 const MarkdownEditor = ({ placeholder = '' }: MarkdownEditorProps): JSX.Element => {
     const [content, setContent] = useState('');
     const debouncedVal = useDebounce(content, 25);
+    const [currContent, undo, redo, set] = useUndoRedo(debouncedVal);
+    const handleKeydown: EventListener = (e: CustomKBEvent) => {
+        const command = getCommand(e);
+        if (command === 'undo') {
+            undo();
+            e.preventDefault();
+        } else if (command === 'redo') {
+            redo();
+            e.preventDefault();
+        }
+    };
+
+    useEffect(() => {
+        if (debouncedVal !== currContent) {
+            set(debouncedVal);
+        }
+    }, [debouncedVal]);
+
+    useEventListener('keydown', handleKeydown);
+
     return (
         <Container>
             <Textarea
                 data-testid="markdown-textarea"
                 onChange={(e: React.ChangeEvent<HTMLTextAreaElement>): void => setContent(e.target.value)}
-                value={debouncedVal}
+                value={currContent}
                 placeholder={placeholder} />
             <MarkdownPreview options={{ forceBlock: true }}>
-                {debouncedVal}
+                {currContent}
             </MarkdownPreview>
         </Container>
     );
