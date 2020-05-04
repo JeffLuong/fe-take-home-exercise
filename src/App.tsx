91611c699e795521f0b4ea4f7d2305034ee6b14c
Jeffrey Luong <jjck1988@gmail.com>
2020-05-04 17:58:05 -0400

    Add some default content styles

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,8 @@ const GlobalStyle = createGlobalStyle`
     }
 
     ::-webkit-scrollbar {
-        width: .625rem; 
+        width: .625rem;
+        height: .625rem;
     }
 
     ::-webkit-scrollbar, ::-webkit-scrollbar-track {
@@ -30,6 +31,40 @@ const GlobalStyle = createGlobalStyle`
     ::-webkit-scrollbar-thumb {
         background-color: #bdbebf;
         border-radius: .25rem;
+    }
+
+    ul {
+        margin: 2rem 0;
+        li:not(:last-of-type) {
+            margin-bottom: .5rem;
+        }
+    }
+
+    code {
+        padding: .15rem .25rem;
+        background-color: #d3d4d4;
+        border-radius: 2px;
+        font-size: .9rem;
+    }
+
+    p {
+        font-size: 1rem;
+        line-height: 1.5;
+        margin: 1.5rem 0;
+
+        code {
+            font-size: 1.125rem;
+        }
+    }
+
+    pre {
+        overflow-x: auto;
+        background-color: #d3d4d4;
+        padding: 1rem;
+
+        code {
+            background-color: transparent;
+        }
     }
 `;
 
